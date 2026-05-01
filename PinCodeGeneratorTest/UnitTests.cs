fcac386da23b726bba4dd31997d21e972a2514d8
xFooFoo <yarivfu@gmail.com>
2026-05-02 10:52:22 +1200

    adding both solution and unit tests

---
+++ PinCodeGeneratorTest/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using PinCodeGeneratorTest;
+
+
+namespace PinCodeGeneratorTest
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void GenerateValidPins_ShouldHaveAtLeast1000Pins()
+        {
+            Assert.True(PinCodeGenerator.GenerateValidPins().Count >= 1000, "There should be at least 1000 valid PIN codes generated.");
+        }
+
+        [Fact]
+        public void GenerateValidPins_ShouldHaveUniquePins()
+        {
+            List<string> validPins = PinCodeGenerator.GenerateValidPins();
+            Assert.Equal(validPins.Distinct().Count(), validPins.Count);
+
+        }
+
+        public void OutputRandomValidPins_ShouldOutputPinsEqualToInput()
+        {
+            List<string> pins = new List<string> { "1234", "5678", "9012" };
+            PinCodeGenerator.OutputRandomValidPins(pins, pins.Count);
+        }
+    }
+}
